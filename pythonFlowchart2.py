a59f80b4ce7c7bafd4b58de2181939e60fd791c8
Mochamad Roiyan <76615805+khaylila@users.noreply.github.com>
2022-06-27 19:05:45 +0700

    Create pythonFlowchart2.py

---
+++ pythonFlowchart2.py
@@ -0,0 +1,15 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print('https://github.com/khaylila/gb-python');
+print("Hello world")
+
+Bilangan = 0;
+penambah = 2;
+i = 0;
+typeInput = int(input("Masukkan bilangan: "));
+
+while i < typeInput:
+    Bilangan += penambah;
+    print(Bilangan);
+    i += 1;
+    
