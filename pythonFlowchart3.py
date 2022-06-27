dd6ee4418849de1772aa06fb17aeb3dc16a653f3
Mochamad Roiyan <76615805+khaylila@users.noreply.github.com>
2022-06-27 20:21:39 +0700

    Create pythonFlowchart3.py
    python mecari bilangan prima

---
+++ pythonFlowchart3.py
@@ -0,0 +1,43 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print('https://github.com/khaylila/gb-python');
+print("Hello world")
+
+n = 1;
+m = int(input("Masukkan nilai M: "));
+# back to this
+
+def function1():
+    # print(n);
+    i = 1;
+    j = 0;
+    
+    index = 0;
+    while i < n:
+        # print(i);
+        if n % i == 0:
+            # print(j);
+            j += 1;
+            # print(j);
+    # if i < n:
+        if index > 0:
+            i += 1;
+        index +=1;
+        
+    if j == 2:
+        print("Bilangan n = ", n);
+   
+# def function2():
+    
+    
+# print(n);
+# print(m);
+while n <= m:
+    function1();
+    n += 1;
+    
+# if n < m:
+#         n += 1;
+#         # back to 1
+#     else:
+#         break;
