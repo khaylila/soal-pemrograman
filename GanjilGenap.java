017fb0f3b1f27a3980df8b38a67e401b527f40eb
Mochamad Roiyan <76615805+khaylila@users.noreply.github.com>
2022-09-11 19:44:22 +0700

    Create GanjilGenap.java

---
+++ GanjilGenap.java
@@ -0,0 +1,11 @@
+public class GanjilGenap {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+        int bilangan = 12;
+        if (bilangan % 2 == 0) {
+            System.out.println("Bilangan " + bilangan + " genap");
+        } else {
+            System.out.println("Bilangan " + bilangan + " ganjil");
+        }
+    }
+}
