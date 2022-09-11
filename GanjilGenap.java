0b81b34a4e3d338533ce0417d819519b04842595
Mochamad Roiyan <76615805+khaylila@users.noreply.github.com>
2022-09-11 20:08:11 +0700

    Update GanjilGenap.java

--- GanjilGenap.java
+++ GanjilGenap.java
@@ -2,10 +2,33 @@ public class GanjilGenap {
     public static void main(String args[]) {
         System.out.println("Hello World!");
         int bilangan = 12;
-        if (bilangan % 2 == 0) {
+        // if (bilangan % 2 == 0) {
+        // System.out.println("Bilangan " + bilangan + " genap");
+        // } else {
+        // System.out.println("Bilangan " + bilangan + " ganjil");
+        // }
+
+        GanjilGenap ganGen = new GanjilGenap();
+        boolean x = ganGen.checkBilangan(bilangan);
+
+        if (x) {
             System.out.println("Bilangan " + bilangan + " genap");
         } else {
             System.out.println("Bilangan " + bilangan + " ganjil");
         }
+
+        // if (checkBilangan(12, "Aku Padamu")) {
+        // System.out.println("Bilangan " + bilangan + " genap");
+        // } else {
+        // System.out.println("Bilangan " + bilangan + " ganjil");
+        // }
+    }
+
+    private boolean checkBilangan(Integer bilangan) {
+        if (bilangan % 2 == 0) {
+            return true;
+        }
+
+        return false;
     }
 }
