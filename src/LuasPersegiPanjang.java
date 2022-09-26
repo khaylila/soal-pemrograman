d2f8132e49690908b9aa07dbed7a26429dfae46d
Mochamad Roiyan <76615805+khaylila@users.noreply.github.com>
2022-09-26 14:29:24 +0700

    Materi Prodas 26 September 2022

---
+++ src/LuasPersegiPanjang.java
@@ -0,0 +1,12 @@
+/**
+ * LuasPersegiPanjang
+ */
+public class LuasPersegiPanjang {
+
+    public static void main(String[] args) {
+        double panjang, lebar;
+        panjang = 10.00;
+        lebar = 8.00;
+        System.out.println("Luas Persegi Panjang adalah " + (panjang * lebar));
+    }
+}
