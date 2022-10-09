197bc97b693f416be25440752bf089e4718023ba
Mochamad Roiyan <76615805+khaylila@users.noreply.github.com>
2022-10-09 20:35:48 +0700

    add looping

---
+++ Pert3/Pert3/src/percobaan2.java
@@ -0,0 +1,9 @@
+public class percobaan2 {
+    public static void main(String[] args) {
+        System.out.println("Nama Buah:");
+        System.out.println("1. Manggis");
+        System.out.println("2. Semangka");
+        System.out.println("3. Sirsak");
+        System.out.println("4. Durian");
+    }
+}
---
+++ Pert3/Pert3/src/tugasPercabangan/ganjilGenap.java
@@ -0,0 +1,19 @@
+package tugasPercabangan;
+
+import java.util.Scanner;
+
+public class ganjilGenap {
+    public static void main(String[] args) {
+        int inputAngka;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("~~~~Program Ganjil Genap~~~~");
+        System.out.print("Masukkan Angka: ");
+        inputAngka = scan.nextInt();
+        if (inputAngka % 2 == 1) {
+            System.out.println("Angka " + inputAngka + " adalah bilangan ganjil");
+        } else {
+            System.out.println("Angka " + inputAngka + " adalah bilangan genap");
+        }
+    }
+}
