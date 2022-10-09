0680231e15ce2b89e3e2c86bdc135cad3bf202ca
Mochamad Roiyan <76615805+khaylila@users.noreply.github.com>
2022-10-09 21:10:37 +0700

    add looping

---
+++ Pert3/src/pengulangan/pengulangan.java
@@ -0,0 +1,53 @@
+package pengulangan;
+
+import java.util.Scanner;
+
+public class pengulangan {
+    public static void main(String[] args) {
+        // for (int i = 0; i <= 10; i++) {
+        // System.out.println("UINSA");
+        // }
+
+        // for (int i = 0; i <= 10; i++) {
+        // System.out.println("*****");
+        // }
+
+        // for (int i = 1; i <= 10; i++) {
+        // System.out.print(i + "");
+        // }
+
+        // for (int i = 1; i <= 20; i += 2) {
+        // System.out.println(i + "");
+        // }
+
+        // while1
+        // Scanner scan = new Scanner(System.in);
+        // boolean running = true;
+        // int counter = 0;
+        // String jawab;
+        // while (running) {
+        // System.out.println("apakah anda ingin keluar? ");
+        // System.out.println("Jawab [ya/tidak]: ");
+        // jawab = scan.nextLine();
+        // if (jawab.equalsIgnoreCase("ya")) {
+        // running = false;
+        // }
+        // counter += 1;
+        // }
+        // System.out.println("Anda melakukan perulangan sebanyak " + counter + "
+        // kali");
+
+        // while2
+        // int i = 0;
+        // while (i <= 10) {
+        // System.out.println("Perulangan ke-" + i);
+        // i++;
+        // }
+
+        int i = 0;
+        do {
+            System.out.println("hujanTurun");
+            i++;
+        } while (i < 5);
+    }
+}
