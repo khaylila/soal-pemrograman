a7c23be9940ac22bfd722fdf3ad9dd0607211647
Mochamad Roiyan <76615805+khaylila@users.noreply.github.com>
2022-10-09 21:09:46 +0700

    Delete Pert3/Pert3 directory

--- Pert3/Pert3/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Nama: Roiyan");
-        System.out.println("Gender: Laki-laki");
-        System.out.println("Alamat: Sidoarjo");
-    }
-}
--- Pert3/Pert3/src/LuasSegitiga.java
+++
@@ -1,8 +0,0 @@
-public class LuasSegitiga {
-    public static void main(String[] args) {
-        double alas = 8, tinggi = 12;
-
-        // luas = alas * tinggi / 2;
-        System.out.println("Luas segitiga adalah " + (alas * tinggi / 2));
-    }
-}
