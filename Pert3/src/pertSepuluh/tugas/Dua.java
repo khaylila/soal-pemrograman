46708733a76b94ababd639a3d463346d4c60ee45
Mochamad Roiyan <76615805+khaylila@users.noreply.github.com>
2022-11-10 13:18:10 +0700

    tugas PErt 10

---
+++ Pert3/src/pertSepuluh/tugas/Dua.java
@@ -0,0 +1,29 @@
+package array.pertSepuluh.tugas;
+
+import java.util.Scanner;
+
+public class Dua {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan jumlah huruf maksimal: ");
+        int maxLength = scan.nextInt();
+        scan.nextLine();
+        int x = 0;
+        String sentence = "";
+        int xx = 0;
+        while (x <= maxLength) {
+            if (xx > 0) {
+                System.out.println("Kata terlalu pendek!");
+            }
+            System.out.print("Masukkan kata anda: ");
+            sentence = scan.nextLine();
+            x = sentence.length();
+            xx++;
+        }
+        String cropSentence = "";
+        for (int i = 0; i < maxLength; i++) {
+            cropSentence += sentence.charAt(i);
+        }
+        System.out.println("huruf anda setelah dipotong '" + cropSentence + "'");
+    }
+}
---
+++ Pert3/src/pertSepuluh/tugas/Satu.java
@@ -0,0 +1,12 @@
+package array.pertSepuluh.tugas;
+
+import java.util.Scanner;
+
+public class Satu {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan kata: ");
+        String word = scan.nextLine();
+        System.out.println(word);
+    }
+}
